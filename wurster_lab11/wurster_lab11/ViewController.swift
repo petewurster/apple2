4d3b12d2695fdd991f101909ca7d6c80e76fad79
petewurster <pwurste1@student.ccp.edu>
2019-11-13 13:23:36 -0500

    11

--- wurster_lab11/wurster_lab11/ViewController.swift
+++ wurster_lab11/wurster_lab11/ViewController.swift
@@ -5,6 +5,7 @@ class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        self.navigationItem.hidesBackButton = true
         // Do any additional setup after loading the view.
     }
 
